033735f8dced7676c7b0f903e454a40e5e36d271
Steve <sw.mahabir@gmail.com>
2015-12-11 11:58:14 -0500

    Added Header Files for Include

---
+++ FileTransfer/Include/ClientSocket.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <string>
+
+// Needed for Sockets
+#include <WinSock2.h>
+#pragma comment (lib,"ws2_32.lib")
+
+using namespace std;
+
+namespace socklib {
+
+	class SocketSender {
+
+	public:
+		SocketSender(std::string ipaddress, unsigned port);
+		~SocketSender();
+
+		std::string sendMessage(std::string message);
+
+		bool SendFile(std::string path, std::string filename);
+
+		SOCKET getSocket();
+
+		enum Type {
+			MESSAGES,
+			FILES
+		};
+
+	private:
+
+		// Socket Connection Managers
+		void Connect();
+		void Disconnect();
+
+		// Core Socket Data
+		SOCKET hSocket;
+		WSAData wsaData;
+
+		// Ip address and Port Number - assigned in the Constructor, never changed!
+		std::string _ip;
+		unsigned _port;
+	};
+
+
+}
---
+++ FileTransfer/Include/ServerSocket.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <string>
+
+// Needed for Sockets
+#include <WinSock2.h>
+#pragma comment (lib,"ws2_32.lib")
+
+
+namespace socklib {
+	 
+	class SocketListener {
+	
+	public:
+
+		// Constructors
+		SocketListener(std::string ipaddress, unsigned port, std::string save_directory);
+		~SocketListener();
+
+		void SetDirectory(std::string directory);
+
+		// Methods
+		std::string Listen();
+
+		enum Type {
+			MESSAGES,
+			FILES
+		};
+
+	private:
+		// Data Members
+		SOCKET hSocket;
+		std::string _ip;
+		unsigned _port;
+		std::string _directory;
+
+	};
+
+}
