78ffbeb2a0ffcb899c681b9b88c814d46a5cb12d
Steve <sw.mahabir@gmail.com>
2015-12-11 18:14:45 -0500

    Commented the Includes

--- FileTransfer/Include/ClientSocket.h
+++ FileTransfer/Include/ClientSocket.h
@@ -1,5 +1,15 @@
 #pragma once
 
+/*
+	ClientSocket.h
+
+	Used for Controlling the Client Socket Calls
+
+	Steve Mahabir and Kevin Postma
+
+	11/Dec/2015
+*/
+
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
 #define _CRT_SECURE_NO_WARNINGS
 
@@ -17,13 +27,17 @@ namespace socklib {
 	class SocketSender {
 
 	public:
+		// Constructor and Destructor
 		SocketSender(std::string ipaddress, unsigned port);
 		~SocketSender();
 
+		// Sends a Message to the Server
 		std::string sendMessage(std::string message);
 
+		// Sends a File to the Server
 		bool SendFile(std::string path, std::string filename);
 
+		// Gets the Current Socket
 		SOCKET getSocket();
 
 		enum Type {
